ff3c090176fc7693c35e58aebd67ca650511ba39
Alexandre Rose <alexrose@debian.gateway.2wire.net>
2013-02-16 16:10:38 -0500

    started coproc

--- TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur.cpp
+++ TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur.cpp
@@ -20,8 +20,26 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-	// Ouverture du/des FIFOs utiles
+	// Ouverture du/des FIFOs utiles	
+	printf("coprocessur %d\n", getpid());
+	
 
+	char message[100];
+	int fd[2];
+	fd[R] = atoi(argv[0]);
+	fd[W] = atoi(argv[1]);
+	int nb;
+	
+	while((nb = read(fd[R], message, 100)) > 0)
+	{
+	close(fd[W]);
+	printf("Message (%d) : %s\n", nb, message); 
+	
+}
+	close(fd[R]);
+	
+	
+	
 	// Réception des instructions
 	//	On traite l'opération (ou on sort de la boucle)
 	//	Si c'était bien une opération, on met à jour l'instruction
--- TP1/inf2610-tp1-hiv2013-1.0.0/src/processeur.cpp
+++ TP1/inf2610-tp1-hiv2013-1.0.0/src/processeur.cpp
@@ -21,6 +21,7 @@
 #include <vector>
 #include <fcntl.h>
 
+
 using namespace std;
 
 
@@ -32,8 +33,79 @@ int main(int argc, char** argv) {
 
 	// On créé la fifo avec laquelle les coprocesseurs pourront communiquer avec le périphérique
 
+	int fd1[2];
+	int fd2[2];
+	int fd3[2];
+	pipe(fd1);
+	pipe(fd2);
+	pipe(fd3);
+	
 	// Création des coprocesseurs
-
+	int p_pid = getpid();
+	printf("proc pid : %d \n", p_pid);
+	int p3, p2, p1;
+	p1 = fork();
+	if (p1 == 0)
+	{
+		char r[33];
+		char w[33];
+		sprintf(r, "%d", fd1[R]);
+		sprintf(w, "%d", fd1[W]);
+		execl("/home/alexrose/Documents/TP-2610/TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur", r, w);
+	}
+	if (getpid() == p_pid)
+	{
+		p2 = fork();
+		if (p2 == 0)
+		{	
+			char r[33];
+			char w[33];
+			sprintf(r, "%d", fd2[R]);
+			sprintf(w, "%d", fd2[W]);
+			execl("/home/alexrose/Documents/TP-2610/TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur", r, w);
+		}
+	}
+	if (getpid() == p_pid)
+	{
+		p3 = fork();
+		if(p3 == 0)
+		{	
+			char r[33];
+			char w[33];
+			sprintf(r, "%d", fd3[R]);
+			sprintf(w, "%d", fd3[W]);
+			execl("/home/alexrose/Documents/TP-2610/TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur", r, w);
+		}
+	}
+	if (getpid() == p_pid)
+		printf("child list is : %d, %d, %d\n", p1, p2, p3);
+		
+	/*if(getpid() != p_pid)
+	{
+		char r[33];
+		char w[33];
+		sprintf(r, "%d", fd[R]);
+		sprintf(w, "%d", fd[W]);
+		
+		
+		execl("/home/alexrose/Documents/TP-2610/TP1/inf2610-tp1-hiv2013-1.0.0/src/coprocesseur", r, w);
+	}*/
+	close(fd2[R]);
+	write(fd2[W], "nothing", strlen("nothing") + 1);
+	close(fd2[W]);
+	
+	close(fd3[R]);
+	write(fd3[W], "nothing", strlen("nothing") + 1);
+	close(fd3[W]);
+	
+	sleep(3);
+	close(fd1[R]);
+	write(fd1[W], "esad", strlen("esad") + 1);
+	sleep(2);
+	write(fd1[W], "esad", strlen("esad") + 1);
+	close(fd1[W]);
+	       
+	
 	// Création du périphérique
 
 	// On entre dans le cycle
@@ -45,7 +117,6 @@ int main(int argc, char** argv) {
 	// On attend qu'ils se terminent
 
 	// On libère les ressources allouées
-
 	return 0;
 }
 
