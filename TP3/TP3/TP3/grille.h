0eec3f2bcb2a9385f4ee20169f7e7ccdab538c9b
Alex-Rose <alexandre.rose.0@gmail.com>
2013-04-04 12:12:13 -0400

    Create TP3

---
+++ TP3/TP3/TP3/grille.h
@@ -0,0 +1,16 @@
+/*
+   * Fichier : grille.h
+   * Auteurs : Alexandre Rose, Anael Maubant
+   * Date : 4 avril 2012
+   * Description : structure représentant la grille
+*/
+
+typedef struct grille_t 
+{
+   int size;
+
+   grille_t()
+   {
+      size = 0;
+   }
+} grille;
---
+++ TP3/TP3/TP3/main.cpp
@@ -0,0 +1,15 @@
+/*
+   * Fichier : main.cpp
+   * Auteurs : Alexandre Rose, Anael Maubant
+   * Date : 4 avril 2012
+   * Description : 
+*/
+#include <iostream>
+#include <Windows.h>
+
+#include "grille.h"
+
+int main()
+{
+   return 0;
+}
